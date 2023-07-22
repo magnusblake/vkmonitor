9f87062f103c896b49f934cad860b9579dfbf427
noleeq <noleeqcode@gmail.com>
2023-07-22 13:48:31 +0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,27 @@
+# [Основные настройки] #
+telegramToken = "6059912059:AAEXlAbB-cv1vY5sM4L1MGbERSP2a2dC1TA"
+telegramChatID = "-666813758"
+vkToken = "d2f6a091d2f6a091d2f6a09192d1e3a572dd2f6d2f6a091b64cc3e7f701c9224d1288c7"
+vkGroupsID = [100155122, 57443638]
+webPagePreview = False
+specialChar = "⠀"
+appLocale = "ru"
+
+# [Настройки API] #
+vkAPIUrl = "https://api.vk.com/method/"
+vkAPIVersion = "5.131"
+
+# [Настройки изображений] #
+imageStart = "images/imageStart.png"
+imageStop = "images/imageStop.png"
+
+# [Настройки базы данных] #
+databaseName = "appDatabase.db"
+
+# [Настройки времени] #
+timeHours = 0
+timeMinutes = 11
+
+# [Настройки текста] #
+captionImageStart = "✅ Мониторинг сообществ был успешно запущен!"
+captionImageStop = "❎  Мониторинг сообществ был успешно остановлен!"
