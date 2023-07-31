ea8454069c3e3f555175cb838bda14a39d7c5d26
noleeq <140243180+noleeq@users.noreply.github.com>
2023-07-31 20:50:41 +0400

    Исправления и улучшения

--- config.py
+++ config.py
@@ -2,31 +2,42 @@
 telegramToken = "telegramToken"
 telegramChatID = "telegramChatID"
 vkToken = "vkToken"
-vkGroupsID = [0000000, 0000001]
-webPagePreview = False
+groupIDs = [110521331, 100155122, 57443638, 48420891, 37182372, 46751044, 72224083, 83538358, 43629833, 59863483, 38298620, 186761818, 61734874, 44073788, 62873115, 101999264, 136914068, 132962673, 42624615]
 specialChar = "⠀"
 appLocale = "ru"
-
-# [Настройки API] #
-vkAPIUrl = "https://api.vk.com/method/"
-vkAPIVersion = "5.131"
 
 # [Настройки изображений] #
 imageStart = "images/imageStart.png"
 imageStop = "images/imageStop.png"
 imagesGroup = {
-    'groupID1': 'images/imageID1.png',
-    'groupID2': 'images/imageID2.png',
-    'groupID3': 'images/imageID3.png'
+    110521331: "images/vitebsk.png",
+    100155122: "images/soligorsk.png",
+    57443638: "images/molodechno.png",
+    48420891: "images/minsk.png",
+    37182372: "images/gomel.png",
+    46751044: "images/jlobin.png",
+    72224083: "images/grodno2.png",
+    83538358: "images/mogilev.png",
+    43629833: "images/orsha.png",
+    59863483: "images/baranovichi.png",
+    38298620: "images/brest.png",
+    186761818: "images/grodno.png",
+    61734874: "images/polock.png",
+    44073788: "images/lida.png",
+    62873115: "images/borisov.png",
+    101999264: "images/mozyr.png",
+    136914068: "images/lelchicy.png",
+    132962673: "images/belarus.png",
+    42624615: "images/polock.png"
 }
 
-# [Настройки базы данных] #
-databaseName = "appDatabase.db"
-
 # [Настройки времени] #
-timeHours = 23
-timeMinutes = 55
+timeHours = 0
+timeMinutes = 1
 
 # [Настройки текста] #
 captionImageStart = "✅ Мониторинг сообществ был успешно запущен!"
-captionImageStop = "❎  Мониторинг сообществ был успешно остановлен!"
+captionImageStop = "🚫  Мониторинг сообществ был успешно остановлен!"
+
+# [Другие системные настройки] #
+typeTrigger = "cron"
